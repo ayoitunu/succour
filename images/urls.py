904fa7cf5d0872dafa83f2334df4132219696d9d
ayoitunu <ayoitunu@yahoo.com>
2020-07-03 18:31:03 +0100

    Serializer setup

--- images/urls.py
+++ images/urls.py
@@ -5,6 +5,10 @@ from . import views
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
+router.register(r'designer', views.DesignerViewSet)
+router.register(r'category', views.CategoryViewSet)
+router.register(r'post', views.PostViewSet)
+
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
--- images/views.py
+++ images/views.py
@@ -1,10 +1,26 @@
-
 from django.contrib.auth.models import User
 from rest_framework import routers, viewsets
-from . import serializers
+from . import serializers, models
+
 
 # ViewSets define the view behavior.
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = serializers.UserSerializer
+
+
+class DesignerViewSet(viewsets.ModelViewSet):
+    queryset = models.Designer.objects.all()
+    serializer_class = serializers.DesignerSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = models.Category.objects.all()
+    serializer_class = serializers.CategorySerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = models.Post.objects.all()
+    serializer_class = serializers.PostSerializer
+
 
